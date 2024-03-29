df67df7d71b3a60fbca3f94671e0087216169c62
Pedro <Pedro-T@users.noreply.github.com>
2024-03-28 22:18:46 -0400

    create spring+react project skeleton

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Group-6
+# Group Six
+
+
+## Concept
+Our app is designed to offer users an interactive way to learn about countries around the world. Through detailed country profiles, and customized search filters, users can explore the diversity of cultures, languages, geographies, and economies globally.
---
+++ src/main/java/net/thingswithstuff/CountryInfoApp/CountryInfoAppApplication.java
@@ -0,0 +1,13 @@
+package net.thingswithstuff.CountryInfoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryInfoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryInfoAppApplication.class, args);
+	}
+
+}
